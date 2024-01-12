79d93a560bfe696adb4500bfa23b0423be4ed5bb
Fenet04 <133098255+Fenet04@users.noreply.github.com>
2024-01-12 22:37:07 +0300

    Add files via upload
    add admin functionalities (create, delete, update, and display the pets)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MulterModule } from '@nestjs/platform-express'; 
 import { AnimalModule } from './modules/animal.module';
-import { AdoptionModule } from './modules/adoption.module'; 
-import { AdopterModule } from './modules/adopter.module';
+
+
 import { Animal, AnimalSchema } from './models/animal.model';
 import { AnimalService } from './services/animal.service';
 import { AnimalController } from './controllers/animal.controller';
@@ -17,8 +17,7 @@ import { memoryStorage } from 'multer';
       storage: memoryStorage(),
     }),
     AnimalModule,
-    AdoptionModule,
-    AdopterModule,
+    
     MongooseModule.forFeature([{ name: Animal.name, schema: AnimalSchema }]),
   ],
   controllers: [AnimalController],
