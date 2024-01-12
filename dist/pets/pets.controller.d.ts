0f6af374ea04ec5299d389d17340c5ca54b55061
Bethel5 <149252303+Bethel5@users.noreply.github.com>
2024-01-12 22:34:31 +0300

    Delete dist directory

--- dist/pets/pets.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PetsService } from './pets.service';
-import { Pet } from './schemas/pet.schema';
-export declare class PetsController {
-    private readonly petsService;
-    constructor(petsService: PetsService);
-    findAll(): Promise<Pet[]>;
-}
